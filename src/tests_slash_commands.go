453b07dab87e379e2dee926f0fcd5c61600af015
GAsNa <romane.leseur.pro@gmail.com>
2023-07-11 15:45:24 +0200

    new init for bot, made for slash commands

---
+++ src/tests_slash_commands.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func slash_command_hello_world(sess *discordgo.Session, i *discordgo.InteractionCreate, data discordgo.ApplicationCommandInteractionData) {
+	err := sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse {
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData {
+			Content: "Hello world!",
+		},
+	},)
+	if err != nil { log.Fatal(err) }
+}
