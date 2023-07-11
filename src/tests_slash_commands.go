e41574078433295c3bac15350f8dcf8a30abb2ec
Rleseur <58465901+GAsNA@users.noreply.github.com>
2023-07-12 00:17:25 +0200

    Blacklist command (#3)
    * slash command blacklist op, but miss privileged and embed for presentation
    
    * embed for blacklisted user wip
    
    * author of the request in the footer
    
    * add timestamp in embed - embed finished

--- src/tests_slash_commands.go
+++ src/tests_slash_commands.go
@@ -8,10 +8,11 @@ import (
 
 func slash_command_hello_world(sess *discordgo.Session, i *discordgo.InteractionCreate, data discordgo.ApplicationCommandInteractionData) {
 	err := sess.InteractionRespond(i.Interaction, &discordgo.InteractionResponse {
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData {
-			Content: "Hello world!",
-		},
-	},)
+				Content: "Hello world!",
+			},
+		},)
 	if err != nil { log.Fatal(err) }
+	//sess.ChannelMessageSend("1127992339227496599", "Hello world!")
 }
