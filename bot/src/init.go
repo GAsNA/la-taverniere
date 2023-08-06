477920d40ea1c457baf848077c0f422a743cc36d
GAsNa <romane.leseur.pro@gmail.com>
2023-08-06 21:28:27 +0200

    move action_db in different file

--- bot/src/init.go
+++ bot/src/init.go
@@ -12,33 +12,10 @@ import (
 	"github.com/uptrace/bun/dialect/sqlitedialect"
 )
 
-type action_db struct {
-	id		int64
-	name	string
-}
-
 var (
 	ctx 	= context.Background()
 	db		*bun.DB
-
-	actions_db = []action_db {
-		{ id: 0, name: "Youtube Video Announcements" },
-		{ id: 0, name: "Youtube Live Announcements" },
-		{ id: 0, name: "Logs" },
-		{ id: 0, name: "Levels" },
-		{ id: 0, name: "Blacklist Logs" },
-	}
 )
-
-func get_action_db_by_name(name string) action_db {
-	for i := 0; i < len(actions_db); i++ {
-		if actions_db[i].name == name {
-			return actions_db[i]
-		}
-	}
-
-	return actions_db[0]
-}
 
 func run_database() {
 	// INIT DB
